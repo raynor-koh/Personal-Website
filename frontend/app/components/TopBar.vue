abf9af8de2b54b718bff36d4c90780a8c7468759
Raynor <64368303+raynor-koh@users.noreply.github.com>
2025-08-10 00:55:20 +0800

    feat: replace home icon with linux root logo

--- frontend/app/components/TopBar.vue
+++ frontend/app/components/TopBar.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
+const route = useRoute();
+
+const displayName = computed(() => {
+  if (!route.name) return ''
+  const name = String(route.name)
+  return name === "index" ? '' : name
+})
 </script>
 
 <template>
   <nav class="top-bar flex items-center justify-between p-4 bg-black">
     <!-- Logo / Home link -->
-    <NuxtLink to="/" class="font-bold text-lg text-white">
-      <UIcon name="material-symbols:home-rounded" class="w-8 h-8 mr-3"/>
-    </NuxtLink>
+    <span class="font-bold text-lg text-white">
+      <NuxtLink to="/" class="inline-block">~/</NuxtLink>{{ displayName }}
+    </span>
 
     <!-- Page links -->
     <ul class="flex space-x-4">
-      <li class="text-white text-lg"><NuxtLink to="/about">About</NuxtLink></li>
-      <li class="text-white text-lg"><NuxtLink to="/projects">Projects</NuxtLink></li>
+      <li class="text-white text-lg">
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li class="text-white text-lg">
+        <NuxtLink to="/projects">Projects</NuxtLink>
+      </li>
     </ul>
-
   </nav>
 </template>
